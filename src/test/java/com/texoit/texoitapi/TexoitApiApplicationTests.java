fc1d322a1f59472e6ac23be0386e52db1be33d13
Fabiano Celestino <facelestino@veltec.com.br>
2022-11-21 00:21:38 -0300

    Versiona pacotes e classes da aplicacao

---
+++ src/test/java/com/texoit/texoitapi/TexoitApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.texoit.texoitapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TexoitApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
